5835ba07d1da35341174895b7afaf02124c0c456
Mille737 <mille737@gmail.com>
2020-04-23 10:58:07 +0200

    write to specific client

--- src/Server/ServerTower.java
+++ src/Server/ServerTower.java
@@ -1,23 +1,33 @@
 package Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerTower {
 
     public static int port = 2911;
-    private Set<UserThreads> userThreads = new HashSet<>();
+    private ArrayList<UserThreads> userThreads = new ArrayList<UserThreads>();
+    public List<UserThreads> getUserThreads() {
+        return userThreads;
+    }
+    private String userName;
 
 
     public void server() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 Socket socket = serverSocket.accept();
+                InputStream inputStream = socket.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                userName = bufferedReader.readLine();
 
-                UserThreads newConnection = new UserThreads(socket,this);
+                UserThreads newConnection = new UserThreads(socket,this, userName);
                 userThreads.add(newConnection);
                 newConnection.start();
             }
--- src/Server/UserThreads.java
+++ src/Server/UserThreads.java
@@ -8,6 +8,7 @@ public class UserThreads extends Thread {
 
     private Socket socket;
     private ServerTower serverTower;
+    private UserThreads userThreads;
     private PrintWriter printWriter;
     private InputStream inputStream;
     private OutputStream outputStream;
@@ -15,9 +16,18 @@ public class UserThreads extends Thread {
     private String userName;
     private static LocalDateTime time;
 
-    public UserThreads(Socket socket, ServerTower serverTower) {
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public UserThreads(Socket socket, ServerTower serverTower, String userName) {
         this.socket = socket;
         this.serverTower = serverTower;
+        this.userName = userName;
     }
 
     public void run() {
@@ -25,7 +35,7 @@ public class UserThreads extends Thread {
             //Input
             inputStream = socket.getInputStream();
             bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            userName = bufferedReader.readLine();
+
             //Output
             outputStream = socket.getOutputStream();
             printWriter = new PrintWriter(outputStream, true);
@@ -34,6 +44,8 @@ public class UserThreads extends Thread {
         }
         if(userName.equals("Gate")) {
             gateProtocol();
+        } else {
+            printWriter.println(userName + " Accepted.");
         }
     }
 
@@ -43,14 +55,14 @@ public class UserThreads extends Thread {
 
         printWriter.println(
                 "Tryk A: Gate crew klar til passagere.\n" +
-                "Tryk B: Boarding påbegyndt.\n" +
-                "Tryk C: Boarding afsluttet.\n" +
-                "Tryk D: Unboarding påbegyndt.\n" +
-                "Tryk E: Unboarding afsluttet. \n" +
-                "Tryk F: Forsinkelse på 10 min.\n" +
-                "Tryk Q: Afslutte kommunikation.");
+                        "Tryk B: Boarding påbegyndt.\n" +
+                        "Tryk C: Boarding afsluttet.\n" +
+                        "Tryk D: Unboarding påbegyndt.\n" +
+                        "Tryk E: Unboarding afsluttet. \n" +
+                        "Tryk F: Forsinkelse på 10 min.\n" +
+                        "Tryk Q: Afslutte kommunikation.");
         try {
-            while(true) {
+            while (true) {
                 choice = bufferedReader.readLine();
                 switch (choice) {
                     case "A":
@@ -72,6 +84,7 @@ public class UserThreads extends Thread {
                     case "E":
                         printWriter.println("Unboarding afsluttet: Accepted. " + time.now());
                         System.out.println("Unboarding afsluttet. " + time.now());
+                        writeToOne("Brændstof", "Påbegynd brændstof påfyldning.");
                         break;
                     case "F":
                         printWriter.println("Forsinkelse på 10 min: Accepted. " + time.now());
@@ -87,6 +100,23 @@ public class UserThreads extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+        public void writeToOne(String userName, String message) {
+            for (int i = 0; i < serverTower.getUserThreads().size(); i++) {
+                userThreads = serverTower.getUserThreads().get(i);
+
+                //if the written username is in list continue
+                if (userName.equalsIgnoreCase(serverTower.getUserThreads().get(i).getUserName())) {
+                    try {
+                        userThreads.printWriter.println(message);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
 }
